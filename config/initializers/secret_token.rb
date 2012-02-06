bee0372c7073d049e1caba3b4de304de12e0ae93
Ted Healey <thealey@gmail.com>
2012-02-06 17:48:42 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hippo::Application.config.secret_token = 'c065033cc41cc3bd0baf90d8fcc5b0a30f2737472c3b9b732a6e2952d1b8b2255b182fe02e627c877b43c30f7a1af6c5d35830525621f7531b2e91967cc50caf'
