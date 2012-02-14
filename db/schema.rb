c8663e7a651010a440518673e8aa211b308f530c
Ryan Tremaine <ryan.trem@gmail.com>
2012-02-14 13:57:19 -0800

    create record and record uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120214215325) do
+ActiveRecord::Schema.define(:version => 20120214215703) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20120214215325) do
     t.string   "file_name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "file"
   end
 
   create_table "users", :force => true do |t|
