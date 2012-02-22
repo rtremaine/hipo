93079701d64afb91a347acf38ab81ba3d4d0e9b6
Ted Healey <thealey@gmail.com>
2012-02-22 13:21:16 -0500

    Pretend stripe file

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,7 +8,7 @@ class Subscription < ActiveRecord::Base
   
   def save_with_payment
     if valid?
-      customer = Stripe::Customer.create(description: self.user.email, plan: plan_id, card: stripe_card_token)
+      customer = Stripe::Customer.create(email: self.user.email, plan: plan_id, card: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
---
+++ config/initializers/stripe_standin.rb
@@ -0,0 +1,2 @@
+Stripe.api_key = ''
+STRIPE_PUBLIC_KEY = 'pk_GoQw1oDiTTjzsO0iAJETrvmw4OdoC'
