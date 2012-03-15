64e8b775872a49778c5f44869ca238395f3c97f2
Ted Healey <thealey@gmail.com>
2012-03-14 22:27:23 -0400

    Working on has for shares

--- app/models/share.rb
+++ app/models/share.rb
@@ -3,4 +3,9 @@ class Share < ActiveRecord::Base
   belongs_to  :record_set
 
   validates_presence_of :record_set
+
+  def sha
+    Digest::SHA1.hexdigest self.recipient.user.username + self.created_at.to_s(:long) + 
+      User.find(self.sender_id).username
+  end
 end
