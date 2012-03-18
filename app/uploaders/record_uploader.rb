632bbaaeb5c88b1ed210eec60eb8d37fe9e343a9
Ryan Tremaine <ryan.trem@gmail.com>
2012-03-18 09:40:32 -0400

    functional file decryption

--- app/uploaders/record_uploader.rb
+++ app/uploaders/record_uploader.rb
@@ -12,7 +12,6 @@ class RecordUploader < CarrierWave::Uploader::Base
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "../private/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
