6efd75adf05c97ffb426f22a32e0f518117c259e
Ryan Tremaine <ryan.trem@gmail.com>
2012-04-20 11:01:26 -0400

    missed this in last checkin, just a code formatting

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,7 @@
 class Patient < ActiveRecord::Base
   belongs_to    :company
-  has_many      :record_sets, :class_name => RecordSet, :finder_sql => proc{"select record_sets.* from record_sets join users on users.id = record_sets.user_id where users.company_id = #{company_id} and patient_id = #{id}"}
+  has_many      :record_sets, :class_name => RecordSet, 
+    :finder_sql => proc{"select record_sets.* from record_sets join users on users.id = record_sets.user_id where users.company_id = #{company_id} and patient_id = #{id}"}
   validates_presence_of   :first, :last
 
   def name
