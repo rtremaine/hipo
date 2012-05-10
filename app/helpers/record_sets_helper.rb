1719c5f3b526ec817a65db4d93f091fdce623049
Ryan Tremaine <ryan.trem@gmail.com>
2012-05-10 12:05:57 -0400

    disable drag and drop for ie browsers

--- app/helpers/record_sets_helper.rb
+++ app/helpers/record_sets_helper.rb
@@ -1,2 +1,9 @@
 module RecordSetsHelper
+  def supports_dnd
+    if browser.ie6? or browser.ie7? or browser.ie8? or browser.ie9? 
+      return false
+    else
+      return true
+    end
+  end
 end
