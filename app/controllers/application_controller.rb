e54f2a6acf98ce3b9d487762d8d42644beb46de2
Ryan Tremaine <ryan.trem@gmail.com>
2012-06-12 14:25:09 -0400

    ok, done for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
-  end 
-  private
+  end
 
+  layout :layout_by_resource
+
+  protected
+  
+  def layout_by_resource
+    if devise_controller? && 
+      (controller_name == 'registrations' || controller_name == 'sessions')
+      "public"
+    else
+      "application"
+    end
+  end
+
+
+  private
+  
   def require_admin
     unless current_user and current_user.is_admin
       flash[:error] = "You must be an administrator to access this section"
